22e33e4371948c4a938334a1c6ba5699dbadcfa2
Mihash08 <mihsasandomirskiy@gmail.com>
2024-04-06 15:57:49 +0200

    namespaces fix

--- ReChargeBackend/Data/AppDbContext.cs
+++ ReChargeBackend/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace SportsStore.Data
+namespace ReCharge.Data
 {
     public class AppDbContext : DbContext
     {
