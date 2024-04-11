4a3027720b6e331291af93c8f28cd0db30ad929d
Mihash08 <mihsasandomirskiy@gmail.com>
2024-04-11 19:20:26 +0200

    CatName on slots added

--- ReChargeBackend/Controllers/SlotController.cs
+++ ReChargeBackend/Controllers/SlotController.cs
@@ -11,116 +11,28 @@ namespace BackendReCharge.Controllers
     public class SlotController : ControllerBase
     {
         private readonly ISlotRepository slotRepository;
-        //List<Slot> slots = new List<Slot>()
-        //    {
-        //        new Slot()
-        //        {
-        //            ActivityId = 1, Id = 1, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 1, Id = 2, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 1, Id = 3, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 2, Id = 4, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 2, Id = 5, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 2, Id = 6, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 3, Id = 7, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 3, Id = 8, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 3, Id = 9, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 4, Id = 10, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 4, Id = 11, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 4, Id = 12, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 5, Id = 13, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 5, Id = 14, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 5, Id = 15, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 6, Id = 16, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 6, Id = 17, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 6, Id = 18, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-
-        //        new Slot()
-        //        {
-        //            ActivityId = 7, Id = 19, FreePlaces = 5, Price = 1500, SlotDateTime = new DateTime(2023,11,20,16,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 7, Id = 20, FreePlaces = 5, Price = 2000, SlotDateTime = new DateTime(2023,11,20,10,30,0)
-        //        },
-        //        new Slot()
-        //        {
-        //            ActivityId = 7, Id = 21, FreePlaces = 5, Price = 1250, SlotDateTime = new DateTime(2023,11,20,18,30,0)
-        //        },
-        //    };
+        private readonly ICategoryRepository categoryRepository;
+       
         private readonly ILogger<SlotController> _logger;
 
-        public SlotController(ILogger<SlotController> logger, ISlotRepository slotRepository)
+        public SlotController(ILogger<SlotController> logger, 
+            ISlotRepository slotRepository,
+            ICategoryRepository categoryRepository)
         {
             _logger = logger;
             this.slotRepository = slotRepository;
+            this.categoryRepository = categoryRepository;
         }
 
         [HttpGet(Name = "GetSlotsByCategory")]
         public IActionResult GetSlotsByCategoryIdAndTime(int categoryId, DateTime dateTime)
         {
             var slots = slotRepository.GetSlotsByCategoryIdAndTime(categoryId, dateTime);
+            var catName = categoryRepository.GetById(categoryId).Name;
             List<GetSlotsByCategoryAndDateResponse> resp = slots.Select(x => new GetSlotsByCategoryAndDateResponse
             {
                 SlotId = x.Id,
                 ActivityName = x.Activity.ActivityName,
-                CategoryName = x.Activity.Category.Name,
                 Address = $"{x.Activity.Location.AddressCity} {x.Activity.Location.AddressStreet} {x.Activity.Location.AddressBuildingNumber}",
                 Coordinates = new ReChargeBackend.Utility.Coordinates
                 {
@@ -135,7 +47,7 @@ namespace BackendReCharge.Controllers
                 Price = x.Price
             }).ToList();
 
-            return Ok(resp);
+            return Ok(new List<Object> { resp, catName });
         }
         [HttpGet(Name = "GetSlotsByActivityIdAndTimeTest")]
         public IActionResult GetSlotsByActivityIdAndTimeTest(int activityId, DateTime dateTime)
