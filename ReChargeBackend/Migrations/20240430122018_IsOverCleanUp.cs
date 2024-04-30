f1e6c618ba3bc46970b09d010425e2109df78c42
Mihash08 <mihsasandomirskiy@gmail.com>
2024-04-30 14:23:38 +0200

    IsOver db update

---
+++ ReChargeBackend/Migrations/20240430122018_IsOverCleanUp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReChargeBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsOverCleanUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_over",
+                table: "reservations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_over",
+                table: "reservations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
