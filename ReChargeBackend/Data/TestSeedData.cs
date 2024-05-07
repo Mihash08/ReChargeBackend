39ad29fcee59c9602609609059cee8bde8819e9e
Mihash08 <mihsasandomirskiy@gmail.com>
2024-05-07 15:55:45 +0200

    Mock user image update

--- ReChargeBackend/Data/TestSeedData.cs
+++ ReChargeBackend/Data/TestSeedData.cs
@@ -33,7 +33,7 @@ namespace ReChargeBackend.Data
                     PhoneNumber = "+79251851096",
                     Surname = "Sandomirskii",
                     City = "Москва",
-                    ImageUrl = "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+                    ImageUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
                 });
             }
 
