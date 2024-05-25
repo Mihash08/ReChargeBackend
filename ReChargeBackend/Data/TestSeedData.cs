48dde3a59da1bb9e1dc04917af254e45955c14f9
Mihash08 <mihsasandomirskiy@gmail.com>
2024-05-25 15:28:54 +0200

    Mock fix

--- ReChargeBackend/Data/TestSeedData.cs
+++ ReChargeBackend/Data/TestSeedData.cs
@@ -349,7 +349,7 @@ namespace ReChargeBackend.Data
                         Id = 16,
                         LocationId = 7,
                         ShouldDisplayWarning = false,
-                        ImageUrl = "https://proftren.ru/assets/images/articles/4093/trainer-min.jpg",
+                        ImageUrl = "https://assets-global.website-files.com/62d964a7de0430c6f4c45556/62e164d7ff091ae19393b01f_MG2_3238%20(1).jpeg",
                         CancelationMessage = "Обратите внимание, отмена доступна не позже, чем за 12 часов",
                     },
                     new Activity()
